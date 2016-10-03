f4500b6c3009f3a392735ac2dcad97b9df82b968
David Pell <davidcpell@gmail.com>
2016-10-03 09:23:52 -0400

    move security groups into own file

---
+++ security_groups.tf
@@ -0,0 +1,78 @@
+resource "aws_security_group" "sg-elb" {
+  name = "test-sg-elb"
+  description = "allow HTTP from anywhere"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "test-sg-elb"
+  }
+}
+
+resource "aws_security_group" "sg-instance" {
+  name = "test-sg-instance"
+  description = "allow HTTP from sg-elb"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.sg-elb.id}"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "test-sg-instance"
+  }
+}
+
+resource "aws_security_group" "sg-db" {
+  name = "test-sg-db"
+  description = "allow db traffic"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.sg-instance.id}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "test-sg-instance"
+  }
+}
