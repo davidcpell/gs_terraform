655563b59b561826711ab3233eb20297085e72c8
David Pell <davidcpell@gmail.com>
2016-10-04 09:43:58 -0400

    move elb into app.tf

--- app.tf
+++ app.tf
@@ -1,3 +1,25 @@
+resource "aws_elb" "elb" {
+  name = "test-elb"
+  subnets = ["${aws_subnet.public1.id}", "${aws_subnet.public2.id}"]
+  cross_zone_load_balancing = true
+  security_groups = ["${aws_security_group.sg-elb.id}"]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "HTTP"
+    lb_port = 80
+    lb_protocol = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    target = "TCP:80"
+    interval = 10
+    timeout = 5
+  }
+}
+
 resource "aws_launch_configuration" "lc" {
   # need an IAM role for RDS
   name_prefix = "test-config-"
